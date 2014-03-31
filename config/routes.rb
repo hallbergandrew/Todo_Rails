93e861fa3952ba19d3ff6965bd272f530cfeaffc
Andrew Hallberg & OKoyea Huff-Boone <student@epicodus.com>
2014-03-31 12:38:40 -0700

    working new, show, and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Todo::Application.routes.draw do
+  match('/', {:via => :get, :to => 'tasks#index'})
+  match('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks', {:via => :post, :to => 'tasks#create'})
+  match('tasks/new', {:via => :get, :to => 'tasks#new'})
+  match('tasks/:id', {:via => :get, :to => 'tasks#show'})
 
 end
